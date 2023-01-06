030180929da83e701e8576c9b713d99c0539ad36
Timon van der Berg <tmnvanderberg@gmail.com>
2023-01-07 00:50:46 +0100

    merge_sort wip

---
+++ programs/1/merge_sort.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+// Merge sort:
+// - divide into n 'lists' of 1 elements
+// - merge adjecent lists until one list is left
+double *merge_sort(double arr[], const int arr_length) { return arr; }
+
+int main() {
+  double arr[11] = {4.2, 2.3, 3.8, 1.2, 9.4, 9.3, 3.2, 4.1, 4.1, 11.9};
+}
